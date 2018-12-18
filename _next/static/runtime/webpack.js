f872b8f3d9e57601a7f57f7c0dd96c1b74464a84
mrmrs <hi@mrmrs.cc>
2018-12-18 16:46:24 +0000

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1f045ffb81ec1ddc9b1f";
+/******/ 	var hotCurrentHash = "728030afafd63beba97d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/index.js.6c3c180e935d5a38e764.hot-update.js
@@ -0,0 +1,29 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./elements/Textarea.js":
+/*!******************************!*\
+  !*** ./elements/Textarea.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react_emotion__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react-emotion */ "./node_modules/react-emotion/dist/index.esm.js");
+/* harmony import */ var styled_system__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-system */ "./node_modules/styled-system/dist/index.esm.js");
+
+
+var Textarea =
+/*#__PURE__*/
+Object(react_emotion__WEBPACK_IMPORTED_MODULE_0__["default"])("textarea", {
+  target: "eckr0fn0"
+})(styled_system__WEBPACK_IMPORTED_MODULE_1__["space"], styled_system__WEBPACK_IMPORTED_MODULE_1__["width"], styled_system__WEBPACK_IMPORTED_MODULE_1__["maxWidth"], styled_system__WEBPACK_IMPORTED_MODULE_1__["fontSize"], styled_system__WEBPACK_IMPORTED_MODULE_1__["color"], styled_system__WEBPACK_IMPORTED_MODULE_1__["borders"], styled_system__WEBPACK_IMPORTED_MODULE_1__["borderColor"], styled_system__WEBPACK_IMPORTED_MODULE_1__["borderRadius"], {
+  overflow: 'auto'
+});
+Textarea.defaultProps = {};
+/* harmony default export */ __webpack_exports__["default"] = (Textarea);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.6c3c180e935d5a38e764.hot-update.js.map
